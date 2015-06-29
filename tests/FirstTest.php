3d7befd499708219b3bbd77361efe95d1e5257cf
Andrey V. Kluev <andrey.kluev@gmail.com>
2015-06-29 17:54:52 +0600

    up

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace andreykluev\finance\tests;
+
+/**
+ * @author Andrey V. Kluev Brandt <mail@cebe.cc>
+ * @group default
+ */
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFirst()
+    {
+    }
+}
